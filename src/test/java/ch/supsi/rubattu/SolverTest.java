9d6cc3542e13b838791eecd091bc2a0f92dc6156
Nicolò Rubattu <nicolo.rubattu@student.supsi.ch>
2020-04-27 19:54:07 +0200

    0.130 Matrin Garrix si volaaaaa + rapporto

--- src/test/java/ch/supsi/rubattu/SolverTest.java
+++ src/test/java/ch/supsi/rubattu/SolverTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 //@Ignore
-public class TestSP {
+public class SolverTest {
 
     private static List<Double> finalRatioResults;
     private final static long MAX_TIME_MILLIS = 181_000;
@@ -21,78 +21,78 @@ public class TestSP {
 
     @Test(timeout = MAX_TIME_MILLIS)
     public void ch130() {
-        // NN + 2h-opt: 1.47
-        String[] args = {"-o -start17 ch130"};
+        // NN + 2h-opt: 1.47 in 350 ms con start = 18
+        String[] args = {"-o -start18 ch130"};
         finalRatioResults.add(new Application(args).run());
-    } // 0
+    } // current best: 0
 
     @Test(timeout = MAX_TIME_MILLIS)
     public void d198() {
-        // NN + 2h-opt: 0.71
-        String[] args = {"-o -start30 d198"};
+        // NN + 2h-opt: 0.71 in 600 ms con start = 31
+        String[] args = {"-o -start31 d198"};
         finalRatioResults.add(new Application(args).run());
-    } // 0
+    } // current best: 0
 
     @Test(timeout = MAX_TIME_MILLIS)
     public void eil76() {
-        // NN + 2h-opt: 0
+        // NN + 2h-opt: 0 in 20 ms con start = 1
         String[] args = {"-o -start1 eil76"};
         finalRatioResults.add(new Application(args).run());
-    } // 0
+    } // current best: 0
 
     @Test(timeout = MAX_TIME_MILLIS)
     public void fl1577() {
-        // NN + 2h-opt: 2.57
+        // NN + 2h-opt: 2.57 in 10 minuti con start = 461
         long seed = 1587216352705L;
         String[] args = {"-o -start461 -seed" + seed + " fl1577"};
         finalRatioResults.add(new Application(args).run());
-    } // 0.409
+    } // current best: 0.409
 
     @Test(timeout = MAX_TIME_MILLIS)
     public void kroA100() {
-        // NN + 2h-opt: 0.18
+        // NN + 2h-opt: 0.18 in 190 ms con start = 17
         String[] args = {"-o -start17 kroA100"};
         finalRatioResults.add(new Application(args).run());
-    } // 0
+    } // current best: 0
 
     @Test(timeout = MAX_TIME_MILLIS)
     public void lin318() {
-        // NN + 2h-opt: 1.69
+        // NN + 2h-opt: 1.69 in 1.9 secondi con start = 97
         String[] args = {"-o -start97 lin318"};
         finalRatioResults.add(new Application(args).run());
-    } // 0
+    } // current best: 0
 
     @Test(timeout = MAX_TIME_MILLIS)
     public void pcb442() {
-        // NN + 2h-opt: 1.22
+        // NN + 2h-opt: 1.22 in 5.7 secondi con start = 221
         long seed = 1586979372812L;
         String[] args = {"-o -start221 -seed" + seed + " pcb442"};
         finalRatioResults.add(new Application(args).run());
-    } // 0
+    } // current best: 0
 
     @Test(timeout = MAX_TIME_MILLIS)
     public void pr439() {
-        // NN + 2h-opt: 1.77
+        // NN + 2h-opt: 1.77 in 5.5 secondi con start = 301
         long seed = 1586023885384L;
         String[] args = {"-o -start301 -seed" + seed + " pr439"};
         finalRatioResults.add(new Application(args).run());
-    } // 0
+    } // current best: 0
 
     @Test(timeout = MAX_TIME_MILLIS)
     public void rat783() {
-        // NN + 2h-opt: 3.83
+        // NN + 2h-opt: 3.83 in 38 secondi con start = 51
         long seed = 1586927476425L;
         String[] args = {"-o -start51 -seed" + seed + " rat783"};
         finalRatioResults.add(new Application(args).run());
-    } // 0.307
+    } // current best: 0.307
 
     @Test(timeout = MAX_TIME_MILLIS)
     public void u1060() {
-        // NN + 2h-opt: 3.78
+        // NN + 2h-opt: 3.78 in 2 minuti con start = 12
         long seed = 1587257994270L;
         String[] args = {"-o -start12 -seed" + seed + " u1060"};
         finalRatioResults.add(new Application(args).run());
-    } // 0.797
+    } // current best: 0.586
 
     @AfterClass
     public static void total() {
